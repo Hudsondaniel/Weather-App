b5f8c186d7a04a5d6d881071565bbb2f6523770c
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2024-07-14 20:00:35 +0530

    custom svg's mapped

--- src/cutomMapping/iconMapping.js
+++ src/cutomMapping/iconMapping.js
@@ -18,6 +18,51 @@ const thunderOutbreak = '../../Assets/weather-icons-dev/production/fill/svg/thun
 const thunderOutbreakNight = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-night.svg'
 const blowingSnow = '../../Assets/weather-icons-dev/production/fill/svg/extreme-day-snow.svg'
 const blowingSnowNight = '../../Assets/weather-icons-dev/production/fill/svg/extreme-night-snow.svg'
+const Blizzard = '../../Assets/weather-icons-dev/production/fill/svg/'
+const BlizzardNight = '../../Assets/weather-icons-dev/production/fill/svg/'
+const fog = '../../Assets/weather-icons-dev/production/fill/svg/fog.svg'
+const fogNight = '../../Assets/weather-icons-dev/production/fill/svg/fog-night.svg'
+const freezingFog = '../../Assets/weather-icons-dev/production/fill/svg/extreme-fog.svg'
+const freezingFogNight = '../../Assets/weather-icons-dev/production/fill/svg/extreme-fog.svg'
+const drizzle = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-day-drizzle.svg'
+const drizzleNight = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-night-drizzle.svg'
+const lightDrizzle = '../../Assets/weather-icons-dev/production/fill/svg/drizzle.svg'
+const lightDrizzleNight = '../../Assets/weather-icons-dev/production/fill/svg/drizzle.svg'
+const freezeDrizzle = '../../Assets/weather-icons-dev/production/fill/svg/overcast-day-drizzle.svg'
+const freezeDrizzleNight = '../../Assets/weather-icons-dev/production/fill/svg/overcast-night-drizzle.svg'
+const heavyDrizzleDay = '../../Assets/weather-icons-dev/production/fill/svg/extreme-day-drizzle.svg'
+const heavyDrizzleNight = '../../Assets/weather-icons-dev/production/fill/svg/extreme-night-drizzle.svg'
+const lightRain = '../../Assets/weather-icons-dev/production/fill/svg/raindrops.svg'
+const lightRainNight = '../../Assets/weather-icons-dev/production/fill/svg/raindrops.svg'
+const moderateRain = '../../Assets/weather-icons-dev/production/fill/svg/rain.svg'
+const heavyRain = '../../Assets/weather-icons-dev/production/fill/svg/extreme-day-rain.svg'
+const heavyRainNight = '../../Assets/weather-icons-dev/production/fill/svg/extreme-night-rain.svg'
+const heavyRainPatch = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-day-rain.svg'
+const heavyRainPatchNight = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-night-rain.svg'
+const sleetLight = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-day-sleet.svg'
+const sleetLightNight = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-night-sleet.svg'
+const moderateSleet = '../../Assets/weather-icons-dev/production/fill/svg/overcast-day-sleet.svg'
+const moderateSleetNight = '../../Assets/weather-icons-dev/production/fill/svg/overcast-night-sleet.svg'
+const snowLight = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-day-snow.svg'
+const snowLightNight = '../../Assets/weather-icons-dev/production/fill/svg/partly-cloudy-night.svg'
+const snowModerate = '../../Assets/weather-icons-dev/production/fill/svg/overcast-day-snow.svg'
+const snowModerateNight = '../../Assets/weather-icons-dev/production/fill/svg/overcast-night-snow.svg'
+const heavySnow = '../../Assets/weather-icons-dev/production/fill/svg/extreme-day-snow.svg'
+const heavySnowNight = '../../Assets/weather-icons-dev/production/fill/svg/extreme-night-snow.svg'
+const thunderRain = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-rain.svg'
+const thunderRainModerate = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-day-rain.svg'
+const thunderRainModerateNight = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-night-rain.svg'
+const thunderRainHeavy = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-day-extreme-rain.svg'
+const thunderRainHeavyNight = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-night-extreme-rain.svg'
+const thunderSnow = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-day-snow.svg'
+const thunderSnowNight = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-night-rain.svg'
+const thunderSnowHeavy = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-day-extreme-snow.svg'
+const thunderSnowHeavyNight = '../../Assets/weather-icons-dev/production/fill/svg/thunderstorms-night-extreme-snow.svg'
+
+
+
+
+
 
 // Still a lot needs to be finished
 
@@ -158,10 +203,16 @@ const weatherIcons = {
         day: "Heavy snow",
         night: "Heavy snow"
     },
+
+    // Same as sleet
+
     "1237": {
         day: "Ice pellets",
         night: "Ice pellets"
     },
+
+    // Use rain svg for showers also.
+
     "1240": {
         day: "Light rain shower",
         night: "Light rain shower"
